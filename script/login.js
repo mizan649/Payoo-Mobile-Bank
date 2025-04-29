4c750140f895a07612a1e6213e647706685e97bc
mizan649 <mizan.softwareengineer@gmail.com>
2025-04-29 23:05:19 +0600

    login page & function done

---
+++ script/login.js
@@ -0,0 +1,20 @@
+
+
+document.getElementById("login-btn").addEventListener("click", 
+       function(event){
+       event.preventDefault();
+       const accountNumber=document.getElementById("account-number").value;
+       const pin=document.getElementById("pin").value;
+       const parsePin=parseInt(pin);
+       if(accountNumber.length==11){
+              if(parsePin===1234){
+                     window.location.href="./main.html"
+              }
+              else{
+                     alert("Pin Unvalid")
+              }
+       }
+       else{
+              alert("Enter Valid Account Number")
+       }
+})
